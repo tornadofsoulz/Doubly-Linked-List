4032de544f35e545764f0ea3cbfee4e0fef090ac
tornadofsoulz <tornadofsoulz@yandex.ru>
2021-09-08 17:03:54 +0300

    Add new Node in Tail side

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::OpenOptions;
+
+pub struct List<T> {
+    tail: Option<Box<Node<T>>>,
+}
+
+struct Node<T> {
+    data: T,
+    next: Option<Box<Node<T>>>,
+    previous: Option<Box<Node<T>>>
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { tail: None }
+    }
+
+    pub fn pushback(&mut self, value: T) {
+        let new_node = Box::new(Node {
+            data: value,
+            next: self.tail.take(),
+            previous: Some(Self)
+        });
+        self.tail = Some(new_node);
+    }
+
+    pub fn rm(&mut self) -> Option<T>{
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.data
+        })
+
+    }
+
 }

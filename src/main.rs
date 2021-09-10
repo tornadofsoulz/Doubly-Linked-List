1f752cd33f284634cb69c7bd68750173c848e0c5
tornadofsoulz <tornadofsoulz@yandex.ru>
2021-09-10 16:28:02 +0300

    todo

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fs::OpenOptions;
 
 pub struct List<T> {
     tail: Option<Box<Node<T>>>,
+    head: Option<Box<Node<T>>>,
 }
 
 struct Node<T> {
@@ -12,24 +13,24 @@ struct Node<T> {
 
 impl<T> List<T> {
     pub fn new() -> Self {
-        List { tail: None }
+        List { tail: None, head: None }
     }
 
     pub fn pushback(&mut self, value: T) {
         let new_node = Box::new(Node {
             data: value,
             next: self.tail.take(),
-            previous: Some(Self)
+            previous: self.head.take()//todo чтобы указывал на предыдущую ноду
         });
         self.tail = Some(new_node);
     }
 
-    pub fn rm(&mut self) -> Option<T>{
+    pub fn rm(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             self.head = node.next;
             node.data
         })
-
     }
-
 }
+
+fn main() {}
